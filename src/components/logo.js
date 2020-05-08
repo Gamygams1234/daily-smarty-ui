de16f4cf28d228ff2de4624159826f9ddf43d07c
Gamygams1234 <gamyburgos@gmail.com>
2020-05-08 01:04:12 -0700

    setup Logo and corresponding styles

---
+++ src/components/logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="logo-main">
+      <img alt="this is the daily smarty logo" src="/assets/ds_circle_logo.png" />
+    </div>
+  );
+}
