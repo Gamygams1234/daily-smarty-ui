01155d11383dbd7cfdb2ddf672bbbe5f6fa2f7ab
Gamygams1234 <gamyburgos@gmail.com>
2020-05-08 17:16:02 -0700

    Setting Up Daily Smarty UI Routes Using React Router Dom

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Logo from "./logo";
 import SearchBar from "./SearchBar";
 import RecentPosts from "./RecentPosts";
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div className="app">
