113d8b020dadc41a21ecba54792b7c466d18fedf
Gamygams1234 <gamyburgos@gmail.com>
2020-05-09 23:38:10 -0700

    Modified Logo styles by using props

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function Logo() {
+export default function Logo(props) {
+  const size = {
+    height: props.size ? props.size : 105,
+    width: props.size ? props.size : 105,
+  };
   return (
     <div className="logo-main">
-      <img alt="this is the daily smarty logo" src="/assets/ds_circle_logo.png" />
+      <img style={size} alt="this is the daily smarty logo" src="/assets/ds_circle_logo.png" />
     </div>
   );
 }
